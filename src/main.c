fa52b500fd94f3c6ddc19ec4a89534ecc351f4b7
cos-overclock <60503661+cos-overclock@users.noreply.github.com>
2022-10-31 13:04:03 +0900

    first commit

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/spi.h"
+#include "pico/binary_info.h"
+#include "arducam.h"
+
+int     main( void ) {
+
+    uint8_t id_H, id_L, spiTestVal;
+    arducam.systemInit( );
+    if ( arducam.busDetect( )) {
+        return 1;
+    }
+    if ( arducam.cameraProbe( )) {
+        return 1;
+    }
+
+    arducam.cameraInit( );
+    arducam.setJpegSize( res_320x240 );
+    while ( true ) {
+        singleCapture( );
+    }
+    return 0;
+}
